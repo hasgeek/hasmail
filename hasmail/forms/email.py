c160591dfdd70805eff9cb9fd1bb4a4d1fb66298
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-12-19 03:06:09 +0530

    Update form imports to Baseframe

--- hasmail/forms/email.py
+++ hasmail/forms/email.py
@@ -1,42 +1,38 @@
-import wtforms
-
-from baseframe.forms import Form, StylesheetField
+import baseframe.forms as forms
 
 from .. import __
 
 __all__ = ['CampaignSettingsForm', 'CampaignSendForm', 'TemplateForm']
 
 
-class CampaignSettingsForm(Form):
-    title = wtforms.TextField(
+class CampaignSettingsForm(forms.Form):
+    title = forms.StringField(
         __("What’s this email about?"),
-        validators=[wtforms.validators.Required(__("This is required"))],
+        validators=[forms.validators.DataRequired(__("This is required"))],
     )
 
-    importfile = wtforms.FileField(
+    importfile = forms.FileField(
         __("Who do you want to send this to?"),
         description=__(
             "A CSV file containing names and email addresses would be most excellent"
         ),
     )
 
-    stylesheet = StylesheetField(
+    stylesheet = forms.StylesheetField(
         __("CSS Stylesheet"),
         description=__("These styles will be applied to your email before it is sent"),
-        validators=[wtforms.validators.Optional()],
+        validators=[forms.validators.Optional()],
     )
 
     # TODO: Validate addresses
-    cc = wtforms.TextAreaField(
-        __("CC"), description=__("CCed recipients, one per line")
-    )
+    cc = forms.TextAreaField(__("CC"), description=__("CCed recipients, one per line"))
 
     # TODO: Validate addresses
-    bcc = wtforms.TextAreaField(
+    bcc = forms.TextAreaField(
         __("BCC"), description=__("BCCed recipients, one per line")
     )
 
-    trackopens = wtforms.BooleanField(
+    trackopens = forms.BooleanField(
         __("Track opens"),
         default=False,
         description=__(
@@ -46,25 +42,25 @@ class CampaignSettingsForm(Form):
         ),
     )
 
-    # trackclicks = wtforms.BooleanField(__(u"Track clicks"), default=False,
+    # trackclicks = forms.BooleanField(__(u"Track clicks"), default=False,
     #     description=__(u"All links in your email will be wrapped and clicks will be tracked "
     #         u"so you know which recipient opened which links"))
 
 
-class CampaignSendForm(Form):
-    email = wtforms.RadioField(
+class CampaignSendForm(forms.Form):
+    email = forms.RadioField(
         __("Send from"),
         description=__("What email address would you like to send this from?"),
     )
 
 
-class TemplateForm(Form):
-    revision_id = wtforms.HiddenField(__("Revision id"))
-    subject = wtforms.TextField(__("Subject"))
+class TemplateForm(forms.Form):
+    revision_id = forms.HiddenField(__("Revision id"))
+    subject = forms.StringField(__("Subject"))
     # Don't use MarkdownField since the only thing it does is adding the
     # 'markdown' class for automatic linking with codemirror, while we
     # want to do that manually with additional options
-    template = wtforms.TextAreaField(__("Template"))
+    template = forms.TextAreaField(__("Template"))
 
     def validate_revision_id(self, field):
         if field.data.isdigit():
