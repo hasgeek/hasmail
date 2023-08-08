3834fcd3323faff48f25ae79f804be7ee1b062a7
Kiran Jonnalagadda <kiran@hasgeek.com>
2023-08-09 01:02:40 +0530

    Load .env files in wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,14 @@
 import os.path
 import sys
 
-sys.path.insert(0, os.path.dirname(__file__))
-
-from hasmail import app as application  # isort:skip
+from flask.cli import load_dotenv
+from flask.helpers import get_load_dotenv
 
 __all__ = ['application']
+
+sys.path.insert(0, os.path.dirname(__file__))
+if get_load_dotenv():
+    load_dotenv()
+
+# pylint: disable=wrong-import-position
+from hasmail import app as application  # isort:skip
